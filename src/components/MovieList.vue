2a53d11e971b6ee016b8d87934a5ebeb4078126d
ctola91 <ctolapacheco@gmail.com>
2022-12-09 00:02:33 -0400

    add styles for flex

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -9,6 +9,8 @@ type Movie = {
 };
 
 const movies = ref<Movie[]>([]);
+const url = Movies.imageURL;
+
 onMounted(async () => {
   const res = await Movies.getTrendingMovies();
 
@@ -19,9 +21,24 @@ onMounted(async () => {
 <template>
   <div>
     <h2>Trending Movies</h2>
-    <div v-for="movie in movies" :key="movie.id">
-      <img :src="movie.poster_path" />
-      <span>{{ movie.title }}</span>
+    <div class="movie-container">
+      <div v-for="movie in movies" :key="movie.id" class="movie-item">
+        <img :src="`${url}/${movie.poster_path}`" />
+        <span>{{ movie.title }}</span>
+      </div>
     </div>
   </div>
 </template>
+<style>
+.movie-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.movie-item {
+  width: 300px;
+}
+.movie-item img {
+    width: 100%;
+    /* max-width: 100%; */
+}
+</style>
--- src/services/Movies.ts
+++ src/services/Movies.ts
@@ -1,10 +1,13 @@
 import API from '@/api/api';
 
+const imageURL = 'https://image.tmdb.org/t/p/w500';
+
 const getTrendingMovies = async () => {
     const response = await API.get(`/trending/all/week`);
     return response.data;
 }
 
 export default {
-    getTrendingMovies
+    getTrendingMovies,
+    imageURL
 };
